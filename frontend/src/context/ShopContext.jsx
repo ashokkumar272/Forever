0c2c1fb810398ce750a6d5cb0650436fd34d4a59
Ashok Kumar <ashokkumar.vg2@gmail.com>
2025-01-15 22:38:09 +0530

    Update ShopContext.jsx

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -10,7 +10,7 @@ export const ShopContext = createContext();
 const ShopContextProvider = (props) => {
   const currency = "$";
   const delivery_fee = 10;
-  const backendUrl = "https://full-stack-backend-79wy.onrender.com"
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [search, setSearch] = useState("");
   const [showSearch, setShowSearch] = useState(false);
   const [cartItems, setCartItems] = useState({});
