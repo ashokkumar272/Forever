5cf28949fc687afb8160cb27669dc290b9ebb7f5
Ashok Kumar <ashokkumar.vg2@gmail.com>
2025-01-15 22:41:26 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -43,7 +43,7 @@ const PlaceOrder = () => {
 
 const initPay = (order) => {
     const options = {
-      key: "rzp_test_hg5hs4UKTLm49u",
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
       amount: order.amount,
       currency: order.currency,
       name: "Order Payment",
